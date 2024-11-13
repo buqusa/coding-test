fc55cc204f6c3386e40a4bfbadfd2d54924491a5
buqusa <165138482+buqusa@users.noreply.github.com>
2024-11-13 09:01:15 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.09 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuffer a=new StringBuffer(my_string);
+        String answer = "";
+        answer=a.reverse().toString();
+        return answer;
+    }
+} //채은이 독감 각이다. 나한테서 떨어져.
